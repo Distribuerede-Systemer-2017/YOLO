a48bdceed3e02db029dda6e4a118200e6a9db94e
Jesper Bruun Hansen <jesperbbhansen@gmail.com>
2017-10-01 10:46:36 +0200

    Create fake database

--- src/main/java/server/endpoints/UserEndpoint.java
+++ src/main/java/server/endpoints/UserEndpoint.java
@@ -1,6 +1,7 @@
 package server.endpoints;
 
 import com.google.gson.Gson;
+import server.database.UserTable;
 import server.models.User;
 
 import javax.ws.rs.*;
@@ -9,12 +10,14 @@ import javax.ws.rs.core.Response;
 @Path("/users")
 public class UserEndpoint {
 
+    UserTable userTable = UserTable.getInstance();
+
     @GET
     public Response getUsers(){
         return Response
                 .status(200)
                 .type("application/json")
-                .entity("{\"userCreated\":\"true\"}")
+                .entity(new Gson().toJson(userTable.getUsers()))
                 .build();
     }
 
@@ -22,6 +25,8 @@ public class UserEndpoint {
     public Response createUser(String jsonUser) {
 
         User user = new Gson().fromJson(jsonUser, User.class);
+        UserTable userTable = UserTable.getInstance();
+        userTable.addUser(user);
 
         return Response
                 .status(200)
