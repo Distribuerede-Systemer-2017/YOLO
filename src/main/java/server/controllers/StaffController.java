59d7ab3ceb9e2627d797bcbf9bc652926046ed1d
felixskarlund <fesk16ab@student.cbs.dk>
2017-10-17 12:29:12 +0200

    StaffController Lavet metoder for StaffController

--- src/main/java/server/controllers/StaffController.java
+++ src/main/java/server/controllers/StaffController.java
@@ -1,5 +1,25 @@
 package server.controllers;
 
+import java.util.ArrayList;
+import server.endpoints.StaffEndpoint;
+import server.models.Order;
+
+/**
+ * Created by Felix on 17-10-2017
+ */
+
 public class StaffController {
-    //Komm
+    public MainController mainController;
+
+    public ArrayList<Order> getOrders(){
+        return StaffEndpoint.getOrders();
+    }
+
+    public void changeOrderStatus(boolean isReady) {
+
+    }
+
+    public void logout(int id){
+        mainController.logout(id);
+    }
 }
--- src/main/java/server/endpoints/StaffEndpoint.java
+++ src/main/java/server/endpoints/StaffEndpoint.java
@@ -1,5 +1,40 @@
 package server.endpoints;
 
-public class StaffEndpoint {
+import com.google.gson.Gson;
+import server.database.DBConnection;
+import server.models.Order;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import server.controllers.StaffController;
+
+public class StaffEndpoint {
+    DBConnection dbCon = new DBConnection();
+    ArrayList<Order> orders = dbCon.getOrders();
+    String ordersAsJson = new Gson().toJson(orders);
+
+    StaffController staffController = new StaffController();
+
+    @PUT
+    @Path("{id}")
+    public Response isReady(@PathParam("id") boolean isReady) {
+
+
+
+        return Response
+                .status(200)
+                .type("application/json")
+                .entity()
+                .build();
+    }
+
+    @GET
+    public Response getOrders() {
+        return Response
+                .status(200)
+                .type("application/json")
+                .entity(ordersAsJson)
+                .build();
+    }
 }
