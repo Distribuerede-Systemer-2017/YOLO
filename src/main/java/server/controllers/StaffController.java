9710b19beca8fd13aa1fdfe00fa9b264f88f62f8
Lasse Munk <lassemunk@mail.com>
2017-10-17 10:05:20 +0200

    tilfoejede javadoc til klassen og metoden deri

--- src/main/java/server/controllers/StaffController.java
+++ src/main/java/server/controllers/StaffController.java
@@ -1,8 +1,18 @@
 package server.controllers;
 
+/**
+ * Class responsible for all logic related to staff operations
+ */
 public class StaffController {
 
-    
+    /**
+     * Returns the list of all orders currently in the database
+     */
+    public void viewOrders() {
+
+
+
+    }
 
 
 }
