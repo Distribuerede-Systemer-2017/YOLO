16bd37d48fc5aae2d30e65cabe0295ef289a9084
Maha16bh <Ubhnqt3m>
2017-10-17 16:00:48 +0200

    Merge branch 'db' of /Users/haestrup/Desktop/YOLO with conflicts.

--- src/main/java/server/database/DBConnection.java
+++ src/main/java/server/database/DBConnection.java
@@ -56,79 +56,37 @@ public class DBConnection {
         return false;
     }
 
-    public ArrayList<Order> findOrderById(int userId) throws Exception{
-        ResultSet resultset = null;
-        ArrayList<Order> orders = null;
 
-        try{
-            PreparedStatement findOrderById = connection.prepareStatement("SELECT * FROM orders WHERE user_userid = ?");
-            findOrderById.setInt(1, userId);
+    public ArrayList<Item> getItems() {
 
-            resultset = findOrderById.executeQuery();
-            while(resultset.next()){
-                Order order = new Order();
-                order.setOrderId(resultset.getInt("order_id"));
-                order.setOrderTime(resultset.getTimestamp("orderTime"));
-                if(resultset.getInt("isReady") != 1){order.isReady(false);} else {order.isReady(true);}
-                order.setUser_userId(resultset.getInt("user_userid"));
+            ResultSet resultSet = null;
+            ArrayList<Item> items = new ArrayList<>();
 
-                orders.add(order);
+            try {
+                PreparedStatement getItems = connection.prepareStatement("SELECT * FROM items");
+                resultSet = getItems.executeQuery();
+
+                while (resultSet.next()) {
+                    Item item = new Item();
+                    item.setItemId(resultSet.getInt("id"));
+                    item.setItemName(resultSet.getString("name"));
+                    item.setItemDescription(resultSet.getString("description"));
+                    item.setItemPrice(resultSet.getInt("price"));
+
+                    items.add(item);
+
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    resultSet.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                    close();
+                }
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+            return items;
         }
-        return orders;
-    }
 
-    public boolean addOrder(User user, ArrayList<Item> items) throws Exception{
-
-        try{
-            PreparedStatement addOrder = connection.prepareStatement("INSERT into orders (userId) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
-            addOrder.setInt(1, user.getUserId());
-            addOrder.executeUpdate();
-            ResultSet rs = addOrder.getGeneratedKeys();
-            rs.next();
-            int orderId = rs.getInt(1);
-
-
-            PreparedStatement addItemsToOrder;
-            for (int i = 0; i < items.size(); i++) {
-                addItemsToOrder = connection.prepareStatement("INSERT into order_has_items (Order_orderId, Items_itemsId) VALUES (?, ?)");
-                addItemsToOrder.setInt(1, orderId);
-                addItemsToOrder.setInt(2, items.get(i).getItemId());
-                addItemsToOrder.executeUpdate();
-            }
-            return true;
-
-        } catch(SQLException e){
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public User authorizeUser(String username, String password) throws Exception {
-        ResultSet resultSet = null;
-        User user = null;
-
-        try {
-            PreparedStatement authorizeUser = connection.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
-
-            authorizeUser.setString(1, username);
-            authorizeUser.setString(2, password);
-
-            resultSet = authorizeUser.executeQuery();
-
-            while (resultSet.next()) {
-                user = new User();
-                user.setUserId(resultSet.getInt("userId"));
-                user.setUsername(resultSet.getString("username"));
-                user.setPassword(resultSet.getString("password"));
-                user.setEmail(resultSet.getString("email"));
-                user.setPersonel(resultSet.getBoolean("isPersonel"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return user;
-    }
 }
