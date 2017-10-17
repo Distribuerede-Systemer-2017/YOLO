545e6721021cc946b57b350eef029293791f8db2
felixskarlund <fesk16ab@student.cbs.dk>
2017-10-17 10:13:43 +0200

    Item-klasse Lavet konstruktør, getters & setters

---
+++ src/main/java/server/models/Item.java
@@ -0,0 +1,61 @@
+package server.models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Felix on 17-10-2017
+ */
+
+public class Item {
+    private int itemId;
+    private String itemName;
+    private String itemDescription;
+    private int itemPrice;
+
+    private ArrayList<Item> items;
+
+    public Items (int itemId, String itemName, String itemDescription, int itemPrice) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.itemPrice = itemPrice;
+
+        this.items = new ArrayList<Item>();
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+}
