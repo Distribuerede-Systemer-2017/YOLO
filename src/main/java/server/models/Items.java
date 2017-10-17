aec7aa1d40fe64adee05218b405d31b93f8965c9
felixskarlund <fesk16ab@student.cbs.dk>
2017-10-17 09:44:06 +0200

    Model-klasse Items

--- src/main/java/server/models/Items.java
+++ src/main/java/server/models/Items.java
@@ -1,4 +1,12 @@
 package server.models;
 
+/**
+ * Created by Felix on 17-10-2017
+ */
+
 public class Items {
+    private int itemId, itemPrice;
+    private String itemName, itemDescription;
 }
+
+
