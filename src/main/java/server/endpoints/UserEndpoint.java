73eff9f4d8a18bdb1526ccc97a2f55f478d6580e
Tobias Walmann Rognstad <tobiaswrognstad@gmail.com>
2017-10-17 11:10:21 +0200

    Lavet alle metoder for UserController.

--- src/main/java/server/endpoints/UserEndpoint.java
+++ src/main/java/server/endpoints/UserEndpoint.java
@@ -1,5 +1,23 @@
 package server.endpoints;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+
+@Path("/user")
 public class UserEndpoint {
 
+
+
+
+
+    @GET
+    public Response getOrdersById(int id){
+
+    }
+
+    @GET
+    public Response getItems(){
+
+    }
 }
