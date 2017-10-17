1d164b79279da6e21cfadcece75ad9d73b5ef194
Maha16bh <Ubhnqt3m>
2017-10-17 10:49:52 +0200

    test

--- src/main/java/server/controllers/MainController.java
+++ src/main/java/server/controllers/MainController.java
@@ -1,4 +1,25 @@
-package server.controllers;
+package main.java.server.controllers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class MainController {
-}
+
+    private User currentUser;
+
+    public MainController (User currentUser){
+        this.currentUser = currentUser;
+    }
+
+
+    private void Login(String username, String password){
+
+
+
+
+    }
+
+    }
+
--- src/main/java/server/models/User.java
+++ src/main/java/server/models/User.java
@@ -24,6 +24,7 @@ public class User {
         this.users = new ArrayList<User>();
     }
 
+
     public User() {
 
     }
