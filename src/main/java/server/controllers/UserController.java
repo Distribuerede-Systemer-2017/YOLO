ca2a72db9352e63f3a4a22f14cb282856ccef0d3
Lasse Munk <lassemunk@mail.com>
2017-10-17 20:22:33 +0200

    tilføjede lidt til UserController, afventer DB merge

--- src/main/java/server/controllers/UserController.java
+++ src/main/java/server/controllers/UserController.java
@@ -1,36 +1,41 @@
 package server.controllers;
 
+import server.database.DBConnection;
 import server.models.*;
 import server.endpoints.UserEndpoint;
 
 import java.util.ArrayList;
 
 public class UserController {
-    public int sessionId;
-    public MainController mainController;
+    private User currentUser;
+    private MainController mainController;
+    private DBConnection dbConnection;
 
 
-    public UserController (int id){
-        setSessionId(id);
+    public UserController (User currentUser){
+        this.currentUser = currentUser;
         this.mainController = new MainController();
+        this.dbConnection = new DBConnection();
     }
 
+    /**
+     * Return the customer's orders
+     * @param id id of the user
+     * @return the ArrayList containing orders
+     */
+    public ArrayList<Order> myOrders(int id){
 
+        //Sørg for at denn metode returnerer et ArrayList
+        return dbConnection.getOrders(id);
 
-    public Order myOrders(int id){
-        return UserEndpoint.getOrdersById(id);
     }
 
     public ArrayList<Item> getItems(){
         return UserEndpoint.getItems();
     }
 
-    public void setSessionId(int sessionId){
-        this.sessionId = sessionId;
-    }
-
-    public void logout(int id){
-        mainController.logout(id);
+    public void logOut() {
+        mainController.logout();
     }
 
 
