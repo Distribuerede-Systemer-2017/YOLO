13582a5baa82ef14b9b44b30ad42c5be5299786d
Tobias Walmann Rognstad <tobiaswrognstad@gmail.com>
2017-10-17 11:12:17 +0200

    Lavet alle metoder for UserController. Rettet og fjernet Endpoint endringer

--- src/main/java/server/endpoints/UserEndpoint.java
+++ src/main/java/server/endpoints/UserEndpoint.java
@@ -1,23 +1,5 @@
 package server.endpoints;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.Response;
-
-
-@Path("/user")
 public class UserEndpoint {
 
-
-
-
-
-    @GET
-    public Response getOrdersById(int id){
-
-    }
-
-    @GET
-    public Response getItems(){
-
-    }
 }
