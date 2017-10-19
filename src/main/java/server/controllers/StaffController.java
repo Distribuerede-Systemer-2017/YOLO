701b860d31422fecefffbe53a94ba7e70d4d1b83
patricklundberg <patrickmlundberg@hotmail.com>
2017-10-19 10:51:51 +0200

    hent alle ordre (y) logud mangler

--- src/main/java/server/controllers/StaffController.java
+++ src/main/java/server/controllers/StaffController.java
@@ -1,17 +1,28 @@
 package server.controllers;
 
+import server.database.DBConnection;
+import server.models.Order;
+import server.endpoints.StaffEndpoint;
+
+import java.util.ArrayList;
+
 /**
  * Class responsible for all logic related to staff operations
  */
 public class StaffController {
+    private DBConnection dbConnection;
+
+    public StaffController(){
+        this.dbConnection = new DBConnection();
+
+    }
 
     /**
      * Returns the list of all orders currently in the database
      */
-    public void viewOrders() {
-
-
-
+    public ArrayList<Order> getOrders() {
+        ArrayList<Order> orders = dbConnection.getOrders();
+        return orders;
     }
 
 
