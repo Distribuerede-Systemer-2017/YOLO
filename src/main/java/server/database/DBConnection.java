f1973b10f225b24657a65e265475ae9817bb9e6c
Maha16bh <Ubhnqt3m>
2017-10-19 13:01:47 +0200

    DBConnection kommentarer tilføjet

--- src/main/java/server/database/DBConnection.java
+++ src/main/java/server/database/DBConnection.java
@@ -57,6 +57,9 @@ public class DBConnection {
 
     }
 
+    /**
+     * Method responsible for terminating the database connection.
+     */
     private static void close() {
         try {
             connection.close();
@@ -65,7 +68,11 @@ public class DBConnection {
         }
     }
 
-    //Begin SQL statements
+    /**
+     * Method reponsible for adding a user to the database.
+     * @param user parameter is inserted into the database using PreparedStatements.
+     * @return returns whether or not the user was added to the database by using "rowsAffected".
+     */
     public boolean addUser(User user) {
         try {
             PreparedStatement addUser = connection.prepareStatement("INSERT into Users (username, password) VALUES (?, ?)");
@@ -83,8 +90,16 @@ public class DBConnection {
         return false;
     }
 
+    /**
+     * Method is responsible for retrieving all orders from the database using PreparedStatements.
+     * @return Returns an ArrayList of Orders from the database.
+     */
     public ArrayList<Order> getOrders() {
 
+        /**
+         * Creates ResultSet to filter through all Orders.
+         * Creates ArrayList which will ultimately be the end product.
+         */
         ResultSet resultSet = null;
         ArrayList<Order> orders = new ArrayList<>();
         ArrayList<Item> itemsInOrder = new ArrayList<>();
@@ -96,7 +111,9 @@ public class DBConnection {
 
             resultSet = getOrders.executeQuery();
 
-            //????
+            /**
+             * While loop that uses resultSet.next to go through each individual Order and add them to the ArrayList.
+             */
             while (resultSet.next()) {
                 Order order = new Order();
                 order.setOrderId(resultSet.getInt("order_id"));
@@ -120,16 +137,27 @@ public class DBConnection {
         return orders;
     }
 
+    /**
+     * Methods responsible for retrieving all items from the database and adding them to an ArrayList.
+     * @return Returns an ArrayList of items from the database.
+     */
     public ArrayList<Item> getItems() {
 
             ResultSet resultSet = null;
             ArrayList<Item> items = new ArrayList<>();
 
-            try {
+        /**
+         * Tries to access the items database using a PreparedStatement.
+         * Uses .excecuteQuery() to go through each item.
+         */
+        try {
                 PreparedStatement getItems = connection.prepareStatement("SELECT * FROM Items");
                 resultSet = getItems.executeQuery();
 
-                while (resultSet.next()) {
+            /**
+             * While loop that uses a resultSet to go through each item and add it to the ArrayList.
+             */
+            while (resultSet.next()) {
                     Item item = new Item();
                     item.setItemId(resultSet.getInt("item_id"));
                     item.setItemName(resultSet.getString("itemName"));
@@ -151,10 +179,18 @@ public class DBConnection {
             return items;
         }
 
+    /**
+     * Method used to find orders from individual users using ID as a primary key.
+     * @param userId Parameter used to determine which users orders are to be retrieved.
+     * @return Returns an ArrayList of orders from the specified user.
+     */
     public ArrayList<Order> findOrderById(int userId) {
         ResultSet resultset = null;
         ArrayList<Order> orders = null;
 
+        /**
+         * Uses PreparedStatement to access the "Orders" database and only selecting the orders where the userid = the parameter.
+         */
         try{
             PreparedStatement findOrderById = connection.prepareStatement("SELECT * FROM Orders WHERE user_userid = ?");
             findOrderById.setInt(1, userId);
@@ -175,8 +211,15 @@ public class DBConnection {
         return orders;
     }
 
+    /**
+     * Method responsible for adding a new order to the database.
+     * @param userId Parameter determining which user issued the specific order.
+     * @param items Parameter determining which item was ordered.
+     * @return Returns whether or not the order was added to the database.
+     */
     public boolean addOrder(int userId, ArrayList<Item> items){
 
+        //Missing comment
         try{
             PreparedStatement addOrder = connection.prepareStatement("INSERT into Orders (userId) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
             addOrder.setInt(1, userId);
@@ -185,6 +228,7 @@ public class DBConnection {
             rs.next();
             int orderId = rs.getInt(1);
 
+            //Missing comment
 
             PreparedStatement addItemsToOrder;
             for (int i = 0; i < items.size(); i++) {
@@ -201,9 +245,18 @@ public class DBConnection {
         return false;
     }
 
+    /**
+     * Method responsible for authorizing a user logging by checking if a user with the given Username & Password exists.
+     * @param controlUser User object containing the given username & password.
+     * @return Returns the user object if found in the database.
+     */
     public User authorizeUser(User controlUser){
         ResultSet resultSet = null;
         User newUser = null;
+
+        /**
+         * Using PreparedStatement to check if any user has the given username & password.
+         */
         try {
             PreparedStatement authorizeUser = connection.prepareStatement("SELECT * FROM Users WHERE username = ? AND password = ?");
 
@@ -212,6 +265,7 @@ public class DBConnection {
 
             resultSet = authorizeUser.executeQuery();
 
+            //Mangler kommentar?
             while (resultSet.next()) {
                 newUser = new User();
                 newUser.setUserId(resultSet.getInt("user_id"));
@@ -231,7 +285,16 @@ public class DBConnection {
         return newUser;
     }
 
+    /**
+     * Method used to change the status of an Order to ready from not ready.
+     * @param orderId Parameter specifying which order is to be made ready.
+     * @return Returns whether the task was completed or not.
+     */
     public boolean makeReady(int orderId){
+
+        /**
+         * Uses PreparedStatement to update a specific order in the database using the given ID and setting isReady to 1 (true).
+         */
         try{
             PreparedStatement makeReady = connection.prepareStatement("UPDATE order SET isReady = 1 WHERE orderID = ?");
             makeReady.setInt(1, orderId);
